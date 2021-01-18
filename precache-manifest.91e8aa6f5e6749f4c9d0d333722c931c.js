fcb16342701ff8c8c72c64e6d6bacccd46c1d81b
MaxAlter <alter0075926@gmail.com>
2021-01-18 17:42:31 +0300

    Updates

---
+++ precache-manifest.91e8aa6f5e6749f4c9d0d333722c931c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4beb419cca8983fc0b55dd921a8d55b6",
+    "url": "/Search-movies/index.html"
+  },
+  {
+    "revision": "e9c1f56828ad3d1d94de",
+    "url": "/Search-movies/static/css/Cast.6b3d5d7b.chunk.css"
+  },
+  {
+    "revision": "6d09c7a56f8164f15ecf",
+    "url": "/Search-movies/static/css/HomePage.aed86824.chunk.css"
+  },
+  {
+    "revision": "e0218fe763dff8e40009",
+    "url": "/Search-movies/static/css/MovieDetailsPage.05cdae21.chunk.css"
+  },
+  {
+    "revision": "eb903250826aec55086b",
+    "url": "/Search-movies/static/css/main.7ed47e5d.chunk.css"
+  },
+  {
+    "revision": "b1645cb8de1033c1ef55",
+    "url": "/Search-movies/static/js/0.45d97c77.chunk.js"
+  },
+  {
+    "revision": "f43ed26e77457edd7e67",
+    "url": "/Search-movies/static/js/8.2efea89b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Search-movies/static/js/8.2efea89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c1f56828ad3d1d94de",
+    "url": "/Search-movies/static/js/Cast.4e73a63f.chunk.js"
+  },
+  {
+    "revision": "6d09c7a56f8164f15ecf",
+    "url": "/Search-movies/static/js/HomePage.603bff6f.chunk.js"
+  },
+  {
+    "revision": "e0218fe763dff8e40009",
+    "url": "/Search-movies/static/js/MovieDetailsPage.8f99fa22.chunk.js"
+  },
+  {
+    "revision": "69781d5e280093d6d4ee",
+    "url": "/Search-movies/static/js/MoviesPage.7431622d.chunk.js"
+  },
+  {
+    "revision": "4c15e45f75f1b8431785",
+    "url": "/Search-movies/static/js/Reviews.dc3222d1.chunk.js"
+  },
+  {
+    "revision": "eb903250826aec55086b",
+    "url": "/Search-movies/static/js/main.e374aeb4.chunk.js"
+  },
+  {
+    "revision": "e1dd16936887db30678e",
+    "url": "/Search-movies/static/js/runtime-main.a691f3ab.js"
+  }
+]);
