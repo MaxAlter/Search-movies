c5fe8aacfbe75153f179fdb636190d05d60a3076
MaxAlter <alter0075926@gmail.com>
2021-01-18 17:38:37 +0300

    ok

---
+++ build/precache-manifest.abdecc98ad1ba5bfb8eaf7aa1c184f26.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a5e6f73764eead9ccbfd02c228b571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c1f56828ad3d1d94de",
+    "url": "/static/css/Cast.6b3d5d7b.chunk.css"
+  },
+  {
+    "revision": "6d09c7a56f8164f15ecf",
+    "url": "/static/css/HomePage.aed86824.chunk.css"
+  },
+  {
+    "revision": "e0218fe763dff8e40009",
+    "url": "/static/css/MovieDetailsPage.05cdae21.chunk.css"
+  },
+  {
+    "revision": "eb903250826aec55086b",
+    "url": "/static/css/main.7ed47e5d.chunk.css"
+  },
+  {
+    "revision": "b1645cb8de1033c1ef55",
+    "url": "/static/js/0.45d97c77.chunk.js"
+  },
+  {
+    "revision": "f43ed26e77457edd7e67",
+    "url": "/static/js/8.2efea89b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/8.2efea89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c1f56828ad3d1d94de",
+    "url": "/static/js/Cast.4e73a63f.chunk.js"
+  },
+  {
+    "revision": "6d09c7a56f8164f15ecf",
+    "url": "/static/js/HomePage.603bff6f.chunk.js"
+  },
+  {
+    "revision": "e0218fe763dff8e40009",
+    "url": "/static/js/MovieDetailsPage.8f99fa22.chunk.js"
+  },
+  {
+    "revision": "69781d5e280093d6d4ee",
+    "url": "/static/js/MoviesPage.7431622d.chunk.js"
+  },
+  {
+    "revision": "4c15e45f75f1b8431785",
+    "url": "/static/js/Reviews.dc3222d1.chunk.js"
+  },
+  {
+    "revision": "eb903250826aec55086b",
+    "url": "/static/js/main.e374aeb4.chunk.js"
+  },
+  {
+    "revision": "360ef2db1df330794b96",
+    "url": "/static/js/runtime-main.d81fca65.js"
+  }
+]);
